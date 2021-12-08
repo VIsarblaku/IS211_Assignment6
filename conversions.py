4e7b86ad3c93e529694de52403a935a4e810e71e
VIsarblaku <62189149+VIsarblaku@users.noreply.github.com>
2021-12-08 18:12:48 +0100

    Add files via upload

--- conversions.py
+++ conversions.py
@@ -1,13 +1,54 @@
-
-def convertCelsiusToKelvin(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"
-    kelvins = 0
-    
-    return kelvins
-
+# -*- coding: utf-8 -*-
+'''
+'''
+#Part I
+'''def convertCelsiusToKelvin(celsius):
+    kelvin = celsius + 273.15
+    return 0.0
 
 def convertCelsiusToFahrenheit(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
-    fahrenheit = 0
-    
+    fahrenheit = (celsius) * 9/5  + 32
+    return 0.0'''
+
+#Part II
+
+def convertCelsiusToKelvin(celsius):
+    kelvin = celsius + 273.15
+    return kelvin
+
+def convertCelsiusfahrenheit(celsius):
+    fahrenheit = (celsius) * 9/5  + 32
+    return fahrenheit
+
+#Part III
+'''def convertFahrenheitToCelsius(fahrenheit):
+    celsius  = (fahrenheit - 32) * 5/9
+    return 0.0
+
+def convertFahrenheitToKelvin(fahrenheit):
+    kelvin = (fahrenheit + 459.67) * 5/9
+    return 0.0
+
+def convertKelvinToCelsius(kelvin):
+    celsius  = kelvin - 273.15
+    return 0.0
+
+def convertKelvinToFahrenheit(kelvin):
+    fahrenheit = (kelvin) * 9/5 - 459.67
+    return 0.0'''
+
+def convertFahrenheitToCelsius(fahrenheit):
+    celsius  = (fahrenheit - 32) * 5/9
+    return celsius
+
+def convertFahrenheitToKelvin(fahrenheit):
+    kelvin = (fahrenheit + 459.67) * 5/9
+    return kelvin
+
+def convertKelvinToCelsius(kelvin):
+    celsius  = kelvin - 273.15
+    return celsius
+
+def convertKelvinToFahrenheit(kelvin):
+    fahrenheit = (kelvin) * 9/5 - 459.67
     return fahrenheit
